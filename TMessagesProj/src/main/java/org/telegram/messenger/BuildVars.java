3fd491c17f384c27ebcb6cedb38499efdcf26466
wrwrabbit <76569803+wrwrabbit@users.noreply.github.com>
2021-01-17 09:43:42 +0300

    Update BuildVars.java
    Remove dev variables

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -20,10 +20,10 @@ public class BuildVars {
     public static boolean CHECK_UPDATES = true;
     public static int BUILD_VERSION = 2206;
     public static String BUILD_VERSION_STRING = "7.3.0";
-    public static int APP_ID = 2150822;
-    public static String APP_HASH = "65adea1743fec62b556d8bd8b91eceb0";
-    public static String APPCENTER_HASH = "a5b5c4f5-51da-dedc-9918-d9766a22ca7c";
-    public static String APPCENTER_HASH_DEBUG = "f9726602-67c9-48d2-b5d0-4761f1c1a8f3";
+    public static int APP_ID = 2150822; // set you own APP_ID
+    public static String APP_HASH = "65adea1743fec62b556d8bd8b91eceb0"; // set you own APP_HASH
+    public static String APPCENTER_HASH = "a5b5c4f5-51da-dedc-9918-d9766a22ca7c"; // unnecessary 
+    public static String APPCENTER_HASH_DEBUG = "f9726602-67c9-48d2-b5d0-4761f1c1a8f3";  // unnecessary 
     //
     public static String SMS_HASH = DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT";
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
