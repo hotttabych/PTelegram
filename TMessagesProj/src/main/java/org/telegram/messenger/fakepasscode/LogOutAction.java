32ae113a62107af6e2c1d2c3de2ad0d15a01017d
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2021-02-14 00:54:41 +0300

    migrate fake passcode

--- TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/LogOutAction.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/LogOutAction.java
@@ -5,6 +5,12 @@ import org.telegram.messenger.MessagesController;
 public class LogOutAction implements Action {
     public int accountNum = 0;
 
+    public LogOutAction() {}
+
+    public LogOutAction(int accountNum) {
+        this.accountNum = accountNum;
+    }
+
     @Override
     public void execute() {
         MessagesController.getInstance(accountNum).performLogout(1);
