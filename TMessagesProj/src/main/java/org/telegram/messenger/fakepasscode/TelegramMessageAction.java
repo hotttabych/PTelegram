61c23271109d76c1a7e0ea1e12754f1dbffdd1f5
ivanov <partizan12589@protonmail.com>
2021-03-13 13:47:45 -0500

    Add removing emergency message

--- TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/TelegramMessageAction.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/TelegramMessageAction.java
@@ -2,16 +2,21 @@ package org.telegram.messenger.fakepasscode;
 
 import org.telegram.messenger.AccountInstance;
 import org.telegram.messenger.ChatObject;
+import org.telegram.messenger.MessageObject;
 import org.telegram.messenger.MessagesController;
+import org.telegram.messenger.MessagesStorage;
 import org.telegram.messenger.SendMessagesHelper;
 import org.telegram.messenger.SharedConfig;
+import org.telegram.messenger.UserConfig;
 import org.telegram.tgnet.TLRPC;
+import org.telegram.ui.Adapters.MessagesSearchAdapter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TelegramMessageAction implements Action {
-    public boolean enabled = false;
     public Map<Integer, String> chatsToSendingMessages = new HashMap<>();
     public int accountNum = 0;
 
@@ -21,9 +26,30 @@ public class TelegramMessageAction implements Action {
             return;
         }
         SendMessagesHelper messageSender = SendMessagesHelper.getInstance(accountNum);
+        MessagesController controller = AccountInstance.getInstance(accountNum).getMessagesController();
         for (Map.Entry<Integer, String> entry : chatsToSendingMessages.entrySet()) {
             messageSender.sendMessage(entry.getValue(), entry.getKey(), null, null, null, false,
                         null, null, null, true, 0);
+            MessageObject obj = null;
+            for (int i = 0; i < controller.dialogMessage.size(); ++i) {
+                if (controller.dialogMessage.valueAt(i).messageText != null &&
+                        entry.getValue().contentEquals(controller.dialogMessage.valueAt(i).messageText)) {
+                    obj = controller.dialogMessage.valueAt(i);
+                    break;
+                }
+            }
+
+            ArrayList<Integer> messages = new ArrayList<>();
+            if (obj != null) {
+                messages.add(obj.getId());
+            }
+            if (entry.getKey() > 0) {
+                controller.deleteMessages(messages, null, null, entry.getKey(),
+                        0, true, false);
+            } else {
+                controller.deleteMessages(messages, null, null, 0,
+                        entry.getKey(), true, false);
+            }
         }
 
         SharedConfig.saveConfig();
