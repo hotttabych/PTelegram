4fc6778b537dff6b732d07e78e80b4858fa450a9
wrwrabbit <76569803+wrwrabbit@users.noreply.github.com>
2021-05-05 20:14:02 +0300

    Update BuildVars.java

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -28,7 +28,7 @@ public class BuildVars {
     public static String SMS_HASH = DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT";
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
 
-    public static String PARTISAN_VERSION_STRING = "2.3.7";
+    public static String PARTISAN_VERSION_STRING = "2.4.0";
 
     static {
         if (ApplicationLoader.applicationContext != null) {
