59e92966b6374cbb38adbe74cfc734cb888db78f
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2021-07-21 12:14:55 +0300

    fix other sessions termination

--- TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/TerminateOtherSessionsAction.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/TerminateOtherSessionsAction.java
@@ -16,19 +16,17 @@ public class TerminateOtherSessionsAction extends AccountAction {
     @Override
     public void execute() {
         TLRPC.TL_auth_resetAuthorizations req = new TLRPC.TL_auth_resetAuthorizations();
-        for (int i = 0; i < UserConfig.MAX_ACCOUNT_COUNT; i++) {
-            ConnectionsManager.getInstance(i).sendRequest(req, (response, error) -> {
-                for (int a = 0; a < UserConfig.MAX_ACCOUNT_COUNT; a++) {
-                    UserConfig userConfig = UserConfig.getInstance(a);
-                    if (!userConfig.isClientActivated()) {
-                        continue;
-                    }
-                    userConfig.registeredForPush = false;
-                    userConfig.saveConfig(false);
-                    MessagesController.getInstance(a).registerForPush(SharedConfig.pushString);
-                    ConnectionsManager.getInstance(a).setUserId(userConfig.getClientUserId());
+        ConnectionsManager.getInstance(accountNum).sendRequest(req, (response, error) -> {
+            for (int a = 0; a < UserConfig.MAX_ACCOUNT_COUNT; a++) {
+                UserConfig userConfig = UserConfig.getInstance(a);
+                if (!userConfig.isClientActivated()) {
+                    continue;
                 }
-            });
-        }
+                userConfig.registeredForPush = false;
+                userConfig.saveConfig(false);
+                MessagesController.getInstance(a).registerForPush(SharedConfig.pushString);
+                ConnectionsManager.getInstance(a).setUserId(userConfig.getClientUserId());
+            }
+        });
     }
 }
