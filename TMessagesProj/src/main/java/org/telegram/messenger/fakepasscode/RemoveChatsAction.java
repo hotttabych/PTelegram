e40a8ddcf565f490e1e44d09b39c2bd2583a4aaa
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2021-10-10 23:18:28 +0300

    fix folders counters

--- TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/RemoveChatsAction.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/RemoveChatsAction.java
@@ -145,6 +145,7 @@ public class RemoveChatsAction extends AccountAction implements NotificationCent
         removedChats = chatEntriesToRemove.stream().filter(e -> e.isExitFromChat && e.isDeleteNewMessages).map(e -> e.chatId).collect(Collectors.toCollection(ArrayList::new));
         hiddenChats = chatEntriesToRemove.stream().filter(e -> !e.isExitFromChat).map(e -> e.chatId).collect(Collectors.toCollection(ArrayList::new));
         SharedConfig.saveConfig();
+        getMessagesStorage().removeChatsActionExecuted();
         notificationCenter.postNotificationName(NotificationCenter.dialogsNeedReload);
     }
 
