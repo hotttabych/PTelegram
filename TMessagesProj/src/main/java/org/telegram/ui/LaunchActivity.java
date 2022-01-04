a1661a899672a022b712bf021b75ac29957ec600
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2022-01-05 00:02:06 +0300

    add SavedChannelsActivity

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -628,6 +628,9 @@ public class LaunchActivity extends Activity implements ActionBarLayout.ActionBa
                 } else if (id == 13) {
                     Browser.openUrl(LaunchActivity.this, LocaleController.getString("TelegramFeaturesUrl", R.string.TelegramFeaturesUrl));
                     drawerLayoutContainer.closeDrawer(false);
+                } else if (id == 100) {
+                    presentFragment(new SavedChannelsActivity(null));
+                    drawerLayoutContainer.closeDrawer(false);
                 }
             }
         });
