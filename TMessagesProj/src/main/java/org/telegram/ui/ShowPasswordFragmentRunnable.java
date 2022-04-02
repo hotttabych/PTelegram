b315017eb9dc134dc76ad9857f783600a3ef456b
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2022-04-02 15:52:48 +0300

    fix two step promt

--- TMessagesProj/src/main/java/org/telegram/ui/ShowPasswordFragmentRunnable.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ShowPasswordFragmentRunnable.java
@@ -2,24 +2,33 @@ package org.telegram.ui;
 
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.Utilities;
+import org.telegram.ui.ActionBar.ActionBarLayout;
 import org.telegram.ui.ActionBar.BaseFragment;
 
 public class ShowPasswordFragmentRunnable  implements Runnable {
     private BaseFragment parentFragment;
     private BaseFragment passwordFragment;
+    private ActionBarLayout parentLayout;
     private int delay;
 
     public ShowPasswordFragmentRunnable(BaseFragment parentFragment, BaseFragment passwordFragment, int delay) {
         this.parentFragment = parentFragment;
         this.passwordFragment = passwordFragment;
+        if (parentFragment != null) {
+            this.parentLayout = parentFragment.getParentLayout();
+        }
         this.delay = delay;
     }
 
     public void run() {
         AndroidUtilities.runOnUIThread(() -> {
-            if (parentFragment.presentFragment(passwordFragment)) {
-                passwordFragment = null;
+            boolean result = false;
+            if (parentFragment.getParentLayout() != null) {
+                result = parentFragment.presentFragment(passwordFragment);
             } else {
+                parentLayout.presentFragment(passwordFragment);
+            }
+            if (!result) {
                 Utilities.globalQueue.postRunnable(new ShowPasswordFragmentRunnable(parentFragment, passwordFragment, delay), delay);
             }
         });
