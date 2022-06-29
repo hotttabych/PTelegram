ebc3269a9ec61656d27578f0acd63204609069c4
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2022-06-29 16:51:04 +0300

    delete archived stickers by action

--- TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/DeleteStickersAction.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/DeleteStickersAction.java
@@ -2,9 +2,11 @@ package org.telegram.messenger.fakepasscode;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.Emoji;
 import org.telegram.messenger.MediaDataController;
 import org.telegram.messenger.NotificationCenter;
+import org.telegram.tgnet.ConnectionsManager;
 import org.telegram.tgnet.TLRPC;
 import org.telegram.ui.StickersActivity;
 
@@ -12,6 +14,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.telegram.messenger.MediaDataController.TYPE_EMOJI;
 import static org.telegram.messenger.MediaDataController.TYPE_IMAGE;
@@ -21,6 +24,8 @@ public class DeleteStickersAction extends AccountAction implements NotificationC
     private Set<Integer> loadedStickerTypes = new HashSet<>();
     @JsonIgnore
     private Set<Long> deletedStickerSets = new HashSet<>();
+    @JsonIgnore
+    private Set<Long> archivedStickerSets = new HashSet<>();
 
     @Override
     public void execute(FakePasscode fakePasscode) {
@@ -34,13 +39,31 @@ public class DeleteStickersAction extends AccountAction implements NotificationC
         for (TLRPC.Document document : MediaDataController.getInstance(accountNum).getRecentGifs()) {
             MediaDataController.getInstance(accountNum).removeRecentGif(document);
         }
-        /*
-        for (int i = 0; i <= TYPE_EMOJI; i++) {
-            MediaDataController.getInstance(accountNum).loadStickers(i, true, false);
-        }
-        */
+        deleteArchivedStickers();
     }
 
+    private void deleteArchivedStickers() {
+        TLRPC.TL_messages_getArchivedStickers req = new TLRPC.TL_messages_getArchivedStickers();
+        req.offset_id = 0;
+        req.limit = 100;
+        req.masks = false;
+        ConnectionsManager.getInstance(accountNum).sendRequest(req, (response, error) -> AndroidUtilities.runOnUIThread(() -> {
+            if (error == null) {
+                processArchivedStickersResponse((TLRPC.TL_messages_archivedStickers) response);
+            }
+        }));
+    }
+
+
+    private void processArchivedStickersResponse(TLRPC.TL_messages_archivedStickers res) {
+        MediaDataController controller = MediaDataController.getInstance(accountNum);
+        for (TLRPC.StickerSetCovered set : res.sets) {
+            controller.toggleStickerSet(null, set, 2, null, false, false);
+        }
+        synchronized (this) {
+            archivedStickerSets = res.sets.stream().map(s -> s.set.id).collect(Collectors.toSet());
+        }
+    }
 
     @Override
     public synchronized void didReceivedNotification(int id, int account, Object... args) {
@@ -53,6 +76,26 @@ public class DeleteStickersAction extends AccountAction implements NotificationC
             MediaDataController controller = MediaDataController.getInstance(accountNum);
             List<TLRPC.TL_messages_stickerSet> stickerSets = new ArrayList<>(controller.getStickerSets(type));
             for (TLRPC.TL_messages_stickerSet stickerSet : stickerSets) {
+                if (!deletedStickerSets.contains(stickerSet.set.id)) {
+                    deletedStickerSets.add(stickerSet.set.id);
+                    controller.toggleStickerSet(null, stickerSet, 0, null, false, false);
+                    synchronized (this) {
+                        archivedStickerSets.remove(stickerSet.set.id);
+                    }
+                }
+            }
+            for (TLRPC.Document document : controller.getRecentStickers(TYPE_IMAGE)) {
+                controller.addRecentSticker(TYPE_IMAGE, null, document, 0, true, false);
+            }
+        } else {
+            MediaDataController controller = MediaDataController.getInstance(accountNum);
+            List<TLRPC.TL_messages_stickerSet> stickerSets = new ArrayList<>(controller.getStickerSets(type));
+            for (TLRPC.TL_messages_stickerSet stickerSet : stickerSets) {
+                synchronized (this) {
+                    if (!archivedStickerSets.contains(stickerSet.set.id)) {
+                        continue;
+                    }
+                }
                 if (!deletedStickerSets.contains(stickerSet.set.id)) {
                     deletedStickerSets.add(stickerSet.set.id);
                     controller.toggleStickerSet(null, stickerSet, 0, null, false, false);
