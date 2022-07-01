bc507a9bb15f293ed7762b52f0b3cd34974819da
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2022-07-01 20:48:57 +0300

    fix TelegramMessageResult

--- TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/TelegramMessageResult.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/fakepasscode/TelegramMessageResult.java
@@ -6,15 +6,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TelegramMessageResult {
-    private List<Pair<Long, Integer>> sosMessagesIds = new ArrayList<>();
+    class Entry {
+        public Entry() {
+        }
+
+        public Entry(long dialogId, int messageId) {
+            this.dialogId = dialogId;
+            this.messageId = messageId;
+        }
+
+        Long dialogId;
+        int messageId;
+    }
+
+    private List<Entry> sosMessagesIds = new ArrayList<>();
 
     public void addMessage(long dialogId, int messageId) {
-        sosMessagesIds.add(new Pair<>(dialogId, messageId));
+        sosMessagesIds.add(new Entry(dialogId, messageId));
     }
 
     public boolean isSosMessage(long dialogId, int messageId) {
-        return sosMessagesIds.stream().anyMatch(p ->
-                (p.first == dialogId || p.first == -dialogId) && p.second == messageId
+        return sosMessagesIds.stream().anyMatch(e ->
+                (e.dialogId == dialogId || e.dialogId == -dialogId) && e.messageId == messageId
         );
     }
 }
