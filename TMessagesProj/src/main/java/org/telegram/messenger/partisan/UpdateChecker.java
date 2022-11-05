6882d3797ae8da2c0c455c376be899e7a3831918
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2022-11-06 00:37:45 +0300

    new update message format

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/UpdateChecker.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/UpdateChecker.java
@@ -1,15 +1,33 @@
 package org.telegram.messenger.partisan;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.os.Build;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextUtils;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.transition.TransitionValues;
+import android.view.View;
+import android.view.ViewGroup;
+
 import org.telegram.messenger.AccountInstance;
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.BuildVars;
+import org.telegram.messenger.Emoji;
+import org.telegram.messenger.LocaleController;
+import org.telegram.messenger.MediaDataController;
 import org.telegram.messenger.MessageObject;
 import org.telegram.messenger.MessagesController;
 import org.telegram.messenger.MessagesStorage;
 import org.telegram.messenger.NotificationCenter;
+import org.telegram.messenger.R;
 import org.telegram.messenger.SharedConfig;
 import org.telegram.tgnet.ConnectionsManager;
 import org.telegram.tgnet.TLRPC;
+import org.telegram.ui.Components.BulletinFactory;
+import org.telegram.ui.Components.StickersAlert;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -17,12 +35,11 @@ import java.util.regex.Pattern;
 
 public class UpdateChecker implements NotificationCenter.NotificationCenterDelegate {
     public interface UpdateCheckedDelegate {
-        void onUpdateResult(boolean updateFounded, UpdateData data);
+        void onUpdateResult(boolean updateFounded, UpdateData update);
     }
 
     private final long CYBER_PARTISAN_SECURITY_TG_CHANNEL_ID = BuildVars.isAlphaApp() ? -1716369838 : -1164492294;  // For checking for updates
     private final String CYBER_PARTISAN_SECURITY_TG_CHANNEL_USERNAME = BuildVars.isAlphaApp() ? "ptg_update_test" : "cpartisans_security";
-    private final String CAN_NOT_SKIP_PREFIX = "IMPORTANT\n";
 
     private boolean partisanTgChannelLastMessageLoaded = false;
     private boolean appUpdatesChecked = false;
@@ -115,38 +132,64 @@ public class UpdateChecker implements NotificationCenter.NotificationCenterDeleg
     }
 
     private void processPartisanTgChannelMessages(ArrayList<MessageObject> messages) {
-        UpdateData data = new UpdateData();
-        Pattern regex = Pattern.compile("PTelegram-v(\\d+)_(\\d+)_(\\d+)(_b)?\\.apk");
+        UpdateData update = new UpdateData();
+        UpdateMessageParser parser = new UpdateMessageParser(currentAccount, getUpdateTgChannelId());
         for (MessageObject message : messages) {
-            AppVersion version = getAppVersionFromMessage(message, regex);
-            if (version != null && (data.version == null || version.greater(data.version))) {
-                data.version = version;
-                data.document = message.getDocument();
-                if (message.caption != null) {
-                    String caption = message.caption.toString();
-                    data.canNotSkip = caption.startsWith(CAN_NOT_SKIP_PREFIX);
-                    data.text = data.canNotSkip
-                            ? caption.substring(CAN_NOT_SKIP_PREFIX.length())
-                            : caption;
-                }
+            UpdateData currentUpdate = parser.parseMessage(message);
+            if (currentUpdate != null && currentUpdate.version != null &&
+                    (update.version == null || currentUpdate.version.greater(update.version))) {
+                update = currentUpdate;
             }
         }
 
-        if (data.version != null && (AppVersion.getCurrentVersion() == null || data.version.greater(AppVersion.getCurrentVersion()))) {
-            delegate.onUpdateResult(true, data);
+        if (update.version != null && (AppVersion.getCurrentVersion() == null || update.version.greater(AppVersion.getCurrentVersion()))) {
+            if (update.stickerPackName != null || update.stickerEmoji != null) {
+                loadSticker(update);
+            } else {
+                delegate.onUpdateResult(true, update);
+            }
         } else {
             delegate.onUpdateResult(false, null);
         }
     }
 
-    private static AppVersion getAppVersionFromMessage(MessageObject message, Pattern regex) {
-        TLRPC.Document doc = message.getDocument();
-        if (doc == null) {
-            return null;
+    private void loadSticker(UpdateData update) {
+        TLRPC.TL_inputStickerSetShortName inputStickerSet = new TLRPC.TL_inputStickerSetShortName();
+        inputStickerSet.short_name = update.stickerPackName != null
+                ? update.stickerPackName
+                : update.stickerEmoji;
+        final MediaDataController mediaDataController = MediaDataController.getInstance(currentAccount);
+        TLRPC.TL_messages_stickerSet stickerSet = null;
+        if (inputStickerSet.short_name != null) {
+            stickerSet = mediaDataController.getStickerSetByName(inputStickerSet.short_name);
         }
-        for (TLRPC.DocumentAttribute attribute : doc.attributes) {
-            if (attribute instanceof TLRPC.TL_documentAttributeFilename) {
-                return AppVersion.parseVersion(attribute.file_name, regex);
+        if (stickerSet == null) {
+            TLRPC.TL_messages_getStickerSet req = new TLRPC.TL_messages_getStickerSet();
+            req.stickerset = inputStickerSet;
+            ConnectionsManager.getInstance(currentAccount).sendRequest(req, (response, error) -> AndroidUtilities.runOnUIThread(() -> {
+                if (error == null) {
+                    TLRPC.TL_messages_stickerSet loadedStickerSet = (TLRPC.TL_messages_stickerSet) response;
+                    update.sticker = getStickerByEmoji(loadedStickerSet, update.stickerEmoji);
+                    delegate.onUpdateResult(true, update);
+                }
+            }));
+        } else {
+            update.sticker = getStickerByEmoji(stickerSet, update.stickerEmoji);
+            delegate.onUpdateResult(true, update);
+        }
+    }
+
+    private TLRPC.Document getStickerByEmoji(TLRPC.TL_messages_stickerSet stickerSet, String emoji) {
+        for (TLRPC.Document document : stickerSet.documents) {
+            for (int a = 0; a < document.attributes.size(); a++) {
+                TLRPC.DocumentAttribute attribute = document.attributes.get(a);
+                if (attribute instanceof TLRPC.TL_documentAttributeSticker) {
+                    if (!TextUtils.isEmpty(attribute.alt)) {
+                        if (attribute.alt.equals(emoji)) {
+                            return document;
+                        }
+                    }
+                }
             }
         }
         return null;
