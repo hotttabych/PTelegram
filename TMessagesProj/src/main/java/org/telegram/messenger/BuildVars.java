bb8394bc2e83bed7ce455ef717ff28442505e5d0
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2023-02-05 02:15:56 +0300

    update to 3.1.2 (1252)

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -38,7 +38,7 @@ public class BuildVars {
     public static String HUAWEI_APP_ID = "101184875";
 
     public static String PARTISAN_VERSION_STRING = "3.1.2";
-    public static int PARTISAN_BUILD_VERSION = 1251;
+    public static int PARTISAN_BUILD_VERSION = 1252;
 
     // You can use this flag to disable Google Play Billing (If you're making fork and want it to be in Google Play)
     public static boolean IS_BILLING_UNAVAILABLE = false;
