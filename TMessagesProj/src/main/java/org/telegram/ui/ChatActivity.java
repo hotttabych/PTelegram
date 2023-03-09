40a9b63d90f97e5c69f1ab872314a7e55a12bed9
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2023-03-09 15:24:23 +0300

    hide save button in private channels

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -3468,7 +3468,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 headerItem.addSubItem(delete_messages, R.drawable.msg_delete, LocaleController.getString(R.string.DeleteMyMessages));
             }
             if (!SharedConfig.isFakePasscodeActivated() && chat != null && SharedConfig.showSavedChannels
-                    && !getUserConfig().isChannelSaved(chat)) {
+                    && !getUserConfig().isChannelSaved(chat) && (chat.username != null || chat.usernames != null && !chat.usernames.isEmpty())) {
                 saveItem = headerItem.lazilyAddSubItem(save, R.drawable.msg_fave, LocaleController.getString("Save", R.string.Save));
             }
         }
