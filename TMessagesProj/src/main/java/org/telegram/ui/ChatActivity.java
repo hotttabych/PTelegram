8bf50860946659d9f7950ba9467df7b8fdbfa9df
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2023-03-13 16:14:02 +0300

    remove save button from topic

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -3472,7 +3472,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     && SharedConfig.showDeleteMyMessages) {
                 headerItem.lazilyAddSubItem(delete_messages, R.drawable.msg_delete, LocaleController.getString(R.string.DeleteMyMessages));
             }
-            if (getUserConfig().isChannelSavingAllowed(chat)) {
+            if (!isTopic && getUserConfig().isChannelSavingAllowed(chat)) {
                 saveItem = headerItem.lazilyAddSubItem(save, R.drawable.msg_fave, LocaleController.getString("Save", R.string.Save));
             }
         }
